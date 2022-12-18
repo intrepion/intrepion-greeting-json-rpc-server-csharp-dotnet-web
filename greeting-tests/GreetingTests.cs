3068026d174e0acdb6caf0392f8547f2a12d3f02
Oliver Forral <intrepion@gmail.com>
2022-12-17 19:02:18 -0700

    Added unhappy paths.

--- greeting-tests/GreetingTests.cs
+++ greeting-tests/GreetingTests.cs
@@ -5,7 +5,19 @@ public class GreetingTests
     [Theory]
     [InlineData("Oliver", "Hello, Oliver!")]
     [InlineData("", "Hello, World!")]
+    [InlineData("😊", "Hello, 😊!")]
     public void HappyPaths(string name, string expected)
+    {
+        var actual = Greeter.Greet(name);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Theory]
+    [InlineData(" Oliver   \n", "Hello, Oliver!")]
+    [InlineData("  \t  \r", "Hello, World!")]
+    [InlineData("\n😊\n😊", "Hello, 😊\n😊!")]
+    public void UnhappyPaths(string name, string expected)
     {
         var actual = Greeter.Greet(name);
 
--- greeting/Greeter.cs
+++ greeting/Greeter.cs
@@ -2,6 +2,8 @@ public class Greeter
 {
     public static string Greet(string name)
     {
+        name = name.Trim();
+
         if (name == "") {
             name = "World";
         }
